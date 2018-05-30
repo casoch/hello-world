ad8a3462c90437c0299d00f66f82a55099847c6d
casoch <carmensolerchorro@enti.cat>
2018-05-30 09:44:54 +0200

    Gerard implementa el registro

--- HelloWorldGithub/Source.cpp
+++ HelloWorldGithub/Source.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
+#include <string>
+
+int Register()
+{
+	std::string username, userpwd;
+	std::cout << "Di tu username ...";
+	std::getline(std::cin, username);
+	std::cout << "Di tu password ...";
+	std::getline(std::cin, userpwd);
+
+	std::cout << "Y aquí Gerard ha programado el registro\n";
+
+	return 0;
+
+}
+
 
 int main()
 {
